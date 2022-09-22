c06961525896fc514a9b62236a19b1ede77603bf
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-22 21:20:23 +0900

    [VerticalStack] スタイル定義

---
+++ src/components/VerticalStack/LayoutContainer.tsx
@@ -0,0 +1,44 @@
+import styled, { css } from 'styled-components'
+import { CharacterProps } from './model'
+
+export const LayoutContainer = styled.div<CharacterProps>`
+  --margin-top: ${({ spaceV, spaceU }) => `${spaceV}${spaceU}`};
+
+  && {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+  }
+
+  && > * {
+    /* 既存の垂直marginを削除 */
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+
+  /* 連続する要素にだけmargin適用 */
+  ${({ recursive }) => {
+    return recursive
+      ? css`
+          && * + * {
+            margin-top: var(--margin-top);
+          }
+        `
+      : css`
+          && > * + * {
+            margin-top: var(--margin-top);
+          }
+        `
+  }}
+
+  ${({ separateFrom }) => {
+    return (
+      separateFrom &&
+      css`
+        && > :nth-child(${separateFrom}) {
+          margin-bottom: auto;
+        }
+      `
+    )
+  }}
+`
