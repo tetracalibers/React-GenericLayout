f61a3d910185d8f21baa74d63e6e0481b4856000
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-22 20:25:28 +0900

    [TwoColumn] スタイル定義

---
+++ src/components/TwoColumn/LayoutContainer.tsx
@@ -0,0 +1,56 @@
+import styled, { css } from 'styled-components'
+import { CharacterProps } from './model'
+
+const css_Variables = css<CharacterProps>`
+  --sub-max-width: ${(props) => props.subMaxWidthV! + props.subMaxWidthU!};
+  --main-min-width: ${(props) => props.mainMinWidthPer! + '%'};
+  --gap: ${(props) => props.gapV! + props.gapU!};
+`
+
+const css_SubColumn = css`
+  flex-basis: var(--sub-max-width);
+  flex-grow: 1;
+`
+
+const css_MainColumn = css`
+  flex-basis: 0;
+  flex-grow: 999;
+  min-width: var(--main-min-width);
+`
+
+export const LayoutContainer = styled.div<CharacterProps>`
+  ${css_Variables}
+
+  && {
+    display: flex;
+    flex-wrap: wrap;
+    gap: var(--gap);
+  }
+
+  ${(props) =>
+    props.noStretchHeight &&
+    css`
+      && {
+        align-items: flex-start;
+      }
+    `}
+
+  ${(props) =>
+    props.subColumn === 'last'
+      ? css`
+          & > :first-child {
+            ${css_MainColumn}
+          }
+          & > :last-child {
+            ${css_SubColumn}
+          }
+        `
+      : css`
+          & > :first-child {
+            ${css_SubColumn}
+          }
+          & > :last-child {
+            ${css_MainColumn}
+          }
+        `}
+`
--- src/components/TwoColumn/model.ts
+++ src/components/TwoColumn/model.ts
@@ -7,22 +7,22 @@ import {
 } from '@polym/react-props'
 import { ElementType } from 'react'
 
-export const subOptions = ['first', 'last'] as const
-export type Sub = typeof subOptions[number]
+export const subColumnOptions = ['first', 'last'] as const
+export type SubColumn = typeof subColumnOptions[number]
 
 const conf = {
   // 2つの子要素のうち、どちらをサイドバーとみなすか
-  sub: NotRequired<Sub>('first'),
+  subColumn: NotRequired<SubColumn>('first'),
   // 水平方向配置におけるメインコンテンツの最小幅[%]
-  mainMinWidthPercent: NotRequired<number>(60),
+  mainMinWidthPer: NotRequired<number>(60),
   // サイドバーとメインコンテンツとの間の隙間
   gapV: NotRequired<number>(1.7),
   gapU: NotRequired<CSSt.Unit.Length>('rem'),
   // 垂直方向配置におけるサイドバーの最大幅
   subMaxWidthV: NotRequired<number>(15),
   subMaxWidthU: NotRequired<CSSt.Unit.Length>('rem'),
-  // falseにすると、垂直方向配置において、要素の高さが引き伸ばされず本来の高さで表示される
-  stretchHeight: NotRequired<boolean>(true)
+  // trueにすると、垂直方向配置において、要素の高さが引き伸ばされず本来の高さで表示される
+  noStretchHeight: NotRequired<boolean>(false)
 }
 
 export type CharacterProps = getPropType<typeof conf>
