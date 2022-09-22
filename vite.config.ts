9e7e5e4ac06d5d5035183c45c3675b1400a6c751
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-22 09:18:45 +0900

    開発server起動時に自動でブラウザを開くように

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	esbuild: {
-		jsxInject: `import React from 'react'`,
-	},
+  plugins: [react()],
+  esbuild: {
+    jsxInject: `import React from 'react'`
+  },
+  server: {
+    open: true
+  }
 })
