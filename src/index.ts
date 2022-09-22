4c8eeb15a259a666dee6baee0f4d7e0af045a255
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-22 22:42:29 +0900

    [SwitchStack] 正常にビルドされることを確認

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { AspectFrame } from './components/AspectFrame'
 import { TwoColumn } from './components/TwoColumn'
 import { VerticalStack } from './components/VerticalStack'
+import { SwitchStack } from './components/SwitchStack'
 
-export { AspectFrame, TwoColumn, VerticalStack }
+export { AspectFrame, TwoColumn, VerticalStack, SwitchStack }
