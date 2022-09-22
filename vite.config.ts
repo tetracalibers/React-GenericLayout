16b90a651057b80f123bafbe68e50a49f7fd65ff
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-22 09:56:06 +0900

    storybook導入によるフォルダ構成変更

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'lib/index.ts'),
+      entry: path.resolve(__dirname, 'src/index.ts'),
       name: '@polym/generic-layout',
       fileName: (format) => `polym-GenericLayout.${format}.js`
     },
