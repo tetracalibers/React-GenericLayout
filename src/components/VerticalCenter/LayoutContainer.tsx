a8996bdf2a5e4fb8d3f7f43a5cf56302e3cc90dd
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-23 12:43:13 +0900

    [VerticalCenter] スタイル定義

---
+++ src/components/VerticalCenter/LayoutContainer.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+import { CharacterProps } from './model'
+
+export const StyledElement = styled.div<CharacterProps>`
+  --space: ${({ spaceV, spaceU }) => `${spaceV}${spaceU}`};
+  --padding: ${({ paddingV, paddingU }) => `${paddingV}${paddingU}`};
+  --min-height: ${({ minHeightV, minHeightU }) => `${minHeightV}${minHeightU}`};
+
+  && {
+    display: flex;
+    flex-direction: column;
+    min-height: var(--min-height);
+    padding: var(--padding);
+    box-sizing: border-box;
+  }
+
+  & > * {
+    /* 各子要素に上下のmarginを設定 */
+    margin-top: var(--space);
+    margin-bottom: var(--space);
+  }
+
+  & > :first-child {
+    margin-top: 0;
+  }
+
+  & > :last-child {
+    margin-bottom: 0;
+  }
+
+  & > :nth-child(${(props) => props.central}) {
+    margin-top: auto;
+    margin-bottom: auto;
+  }
+`
