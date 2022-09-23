5723c36b2cb04cfc1ca05e1b7be8f6f0fbd7ee0f
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-24 00:21:09 +0900

    Updates

---
+++ assets/HorizontalCenter.stories.dae9c09e.js
@@ -0,0 +1,96 @@
+import{r as c,j as i}from"./jsx-runtime.19baf7d1.js";import{s as d,C as u,o as v,l as t,K as n,r as s,d as f,a as h,b as x}from"./ref.d133f60d.js";import"./iframe.a015d09f.js";const o=d.div`
+  --paddingX: ${e=>e.paddingXV+e.paddingXU};
+  --max-width: ${e=>e.maxWidthV+e.maxWidthU};
+
+  ${({byContentWidth:e})=>e&&u`
+      && {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+    `}
+
+  ${({textCenter:e})=>e&&u`
+      && {
+        text-align: center;
+      }
+    `}
+  
+  && {
+    box-sizing: border-box;
+    padding-left: var(--paddingX);
+    padding-right: var(--paddingX);
+  }
+
+  && > * {
+    /* paddingを幅の計算から除外 */
+    box-sizing: content-box;
+    max-width: var(--max-width);
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;try{o.displayName="LayoutContainer",o.__docgenInfo={description:"",displayName:"LayoutContainer",props:{ref:{defaultValue:null,description:"",name:"ref",required:!1,type:{name:"((instance: HTMLDivElement | null) => void) | RefObject<HTMLDivElement> | null"}},textCenter:{defaultValue:null,description:"",name:"textCenter",required:!1,type:{name:"boolean"}},byContentWidth:{defaultValue:null,description:"",name:"byContentWidth",required:!1,type:{name:"boolean"}},paddingXV:{defaultValue:null,description:"",name:"paddingXV",required:!1,type:{name:"number"}},paddingXU:{defaultValue:null,description:"",name:"paddingXU",required:!1,type:{name:"enum",value:[{value:'"em"'},{value:'"px"'},{value:'"ex"'},{value:'"ch"'},{value:'"rem"'},{value:'"vw"'},{value:'"vh"'},{value:'"vmin"'},{value:'"vmax"'},{value:'"vb"'},{value:'"vi"'},{value:'"svw"'},{value:'"svh"'},{value:'"lvw"'},{value:'"lvh"'},{value:'"dvw"'},{value:'"dvh"'}]}},maxWidthV:{defaultValue:null,description:"",name:"maxWidthV",required:!1,type:{name:"number"}},maxWidthU:{defaultValue:null,description:"",name:"maxWidthU",required:!1,type:{name:"enum",value:[{value:'"em"'},{value:'"px"'},{value:'"ex"'},{value:'"ch"'},{value:'"rem"'},{value:'"vw"'},{value:'"vh"'},{value:'"vmin"'},{value:'"vmax"'},{value:'"vb"'},{value:'"vi"'},{value:'"svw"'},{value:'"svh"'},{value:'"lvw"'},{value:'"lvh"'},{value:'"dvw"'},{value:'"dvh"'}]}},theme:{defaultValue:null,description:"",name:"theme",required:!1,type:{name:"any"}},as:{defaultValue:null,description:"",name:"as",required:!1,type:{name:"undefined"}},forwardedAs:{defaultValue:null,description:"",name:"forwardedAs",required:!1,type:{name:"undefined"}}}},typeof STORYBOOK_REACT_CLASSES<"u"&&(STORYBOOK_REACT_CLASSES["src/components/HorizontalCenter/LayoutContainer.tsx#LayoutContainer"]={docgenInfo:o.__docgenInfo,name:"LayoutContainer",path:"src/components/HorizontalCenter/LayoutContainer.tsx#LayoutContainer"})}catch{}const y={textCenter:t(!1),byContentWidth:t(!1),paddingXV:t(1),paddingXU:t("rem"),maxWidthV:t(30),maxWidthU:t("rem")},a=v(y),g=({ref:e,as:r,children:p,...m}={...a})=>i(o,{...m,ref:e,as:r||"div",children:p}),l=c.exports.forwardRef(g);try{l.displayName="HorizontalCenter",l.__docgenInfo={description:"",displayName:"HorizontalCenter",props:{textCenter:{defaultValue:null,description:"",name:"textCenter",required:!1,type:{name:"boolean"}},byContentWidth:{defaultValue:null,description:"",name:"byContentWidth",required:!1,type:{name:"boolean"}},paddingXV:{defaultValue:null,description:"",name:"paddingXV",required:!1,type:{name:"number"}},paddingXU:{defaultValue:null,description:"",name:"paddingXU",required:!1,type:{name:"enum",value:[{value:'"em"'},{value:'"px"'},{value:'"ex"'},{value:'"ch"'},{value:'"rem"'},{value:'"vw"'},{value:'"vh"'},{value:'"vmin"'},{value:'"vmax"'},{value:'"vb"'},{value:'"vi"'},{value:'"svw"'},{value:'"svh"'},{value:'"lvw"'},{value:'"lvh"'},{value:'"dvw"'},{value:'"dvh"'}]}},maxWidthV:{defaultValue:null,description:"",name:"maxWidthV",required:!1,type:{name:"number"}},maxWidthU:{defaultValue:null,description:"",name:"maxWidthU",required:!1,type:{name:"enum",value:[{value:'"em"'},{value:'"px"'},{value:'"ex"'},{value:'"ch"'},{value:'"rem"'},{value:'"vw"'},{value:'"vh"'},{value:'"vmin"'},{value:'"vmax"'},{value:'"vb"'},{value:'"vi"'},{value:'"svw"'},{value:'"svh"'},{value:'"lvw"'},{value:'"lvh"'},{value:'"dvw"'},{value:'"dvh"'}]}},as:{defaultValue:null,description:"",name:"as",required:!1,type:{name:"ElementType<any>"}},ref:{defaultValue:null,description:"",name:"ref",required:!1,type:{name:"any"}}}},typeof STORYBOOK_REACT_CLASSES<"u"&&(STORYBOOK_REACT_CLASSES["src/components/HorizontalCenter/index.tsx#HorizontalCenter"]={docgenInfo:l.__docgenInfo,name:"HorizontalCenter",path:"src/components/HorizontalCenter/index.tsx#HorizontalCenter"})}catch{}const C={paddingXV:{...n.padding,control:{type:"number"},description:"Space at both ends of content",table:{...n.padding.table,defaultValue:{summary:a.paddingXV,details:null}}},paddingXU:{...n.padding,control:{type:"select"},options:s.Unit.length,description:"Units of paddingXV",table:{...n.padding.table,defaultValue:{summary:a.paddingXU,details:null}}},maxWidthV:{...n.maxWidth,control:{type:"number"},description:"Space at both ends of content",table:{...n.maxWidth.table,defaultValue:{summary:a.maxWidthV,details:null}}},maxWidthU:{...n.maxWidth,control:{type:"select"},options:s.Unit.length,description:"Units of maxWidthV",table:{...n.maxWidth.table,defaultValue:{summary:a.maxWidthU,details:null}}},textCenter:{...n.textAlign,control:{type:"boolean"},description:"Whether text is also centered or not",table:{...n.textAlign.table,defaultValue:{summary:a.textCenter,details:null}}},byContentWidth:{control:{type:"boolean"},description:"Whether to center child elements based on their content width",table:{type:{summary:null},category:"style",defaultValue:{summary:a.byContentWidth,details:null}}}},S={title:"layout/HorizontalCenter",component:l,parameters:{storySource:{source:`import { ComponentStory } from '@storybook/react'
+import { HorizontalCenter } from '../components/HorizontalCenter'
+import { defaultProps } from '../components/HorizontalCenter/model'
+import { desc_HorizontalCenterProps } from './args/HorizontalCenter'
+import { desc_as } from './args/common/as'
+import { desc_children } from './args/common/children'
+import { desc_ref } from './args/common/ref'
+import styled from 'styled-components'
+import React from 'react'
+
+export default {
+  title: 'layout/HorizontalCenter',
+  component: HorizontalCenter,
+  parameters: {
+    docs: {
+      description: {
+        component: 'Suitable solution for centering elements horizontally'
+      }
+    }
+  },
+  argTypes: {
+    ...desc_as,
+    ...desc_ref,
+    ...desc_children,
+    ...desc_HorizontalCenterProps
+  }
+}
+
+const FullWidthContainer = styled.div\`
+  width: 100%;
+  color: #4d608b;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px,
+    rgba(0, 0, 0, 0.22) 0px 10px 10px;
+\`
+
+const Item = styled.div\`
+  box-shadow: rgba(0, 0, 0, 0.04) 0px 3px 5px;
+  background-color: #a2a7fe66;
+  height: 100px;
+\`
+
+const Template: ComponentStory<typeof HorizontalCenter> = ({
+  children,
+  ...args
+}) => (
+  <HorizontalCenter {...args} as={FullWidthContainer}>
+    <Item>Centering!!</Item>
+  </HorizontalCenter>
+)
+
+export const playground = Template.bind({})
+playground.args = {
+  ...defaultProps
+}
+`,locationsMap:{playground:{startLoc:{col:58,line:42},endLoc:{col:1,line:49},startBody:{col:58,line:42},endBody:{col:1,line:49}}}},docs:{description:{component:"Suitable solution for centering elements horizontally"}}},argTypes:{...f,...h,...x,...C}},b=d.div`
+  width: 100%;
+  color: #4d608b;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px,
+    rgba(0, 0, 0, 0.22) 0px 10px 10px;
+`,_=d.div`
+  box-shadow: rgba(0, 0, 0, 0.04) 0px 3px 5px;
+  background-color: #a2a7fe66;
+  height: 100px;
+`,V=({children:e,...r})=>i(l,{...r,as:b,children:i(_,{children:"Centering!!"})}),W=V.bind({});W.args={...a};const U=["playground"];export{U as __namedExportsOrder,S as default,W as playground};
+//# sourceMappingURL=HorizontalCenter.stories.dae9c09e.js.map
---
+++ assets/TwoColumn.stories.58ecf3a6.js
@@ -0,0 +1,115 @@
+import{r as m,j as t,b}from"./jsx-runtime.19baf7d1.js";import{C as o,s as i,o as y,l,K as n,r as d,d as C,a as x,b as w}from"./ref.d133f60d.js";import{d as v}from"./index.4538ee3e.js";import"./iframe.a015d09f.js";import"./index.3bc3f484.js";const g=o`
+  --sub-max-width: ${e=>e.subMaxWidthV+e.subMaxWidthU};
+  --main-min-width: ${e=>e.mainMinWidthPer+"%"};
+  --gap: ${e=>e.spaceV+e.spaceU};
+`,c=o`
+  flex-basis: var(--sub-max-width);
+  flex-grow: 1;
+`,p=o`
+  flex-basis: 0;
+  flex-grow: 999;
+  min-width: var(--main-min-width);
+`,s=i.div`
+  ${g}
+
+  && {
+    display: flex;
+    flex-wrap: wrap;
+    gap: var(--gap);
+  }
+
+  ${e=>e.noStretchHeight&&o`
+      && {
+        align-items: flex-start;
+      }
+    `}
+
+  ${e=>e.subColumn==="last"?o`
+          & > :first-child {
+            ${p}
+          }
+          & > :last-child {
+            ${c}
+          }
+        `:o`
+          & > :first-child {
+            ${c}
+          }
+          & > :last-child {
+            ${p}
+          }
+        `}
+`;try{s.displayName="LayoutContainer",s.__docgenInfo={description:"",displayName:"LayoutContainer",props:{ref:{defaultValue:null,description:"",name:"ref",required:!1,type:{name:"((instance: HTMLDivElement | null) => void) | RefObject<HTMLDivElement> | null"}},spaceV:{defaultValue:null,description:"",name:"spaceV",required:!1,type:{name:"number"}},spaceU:{defaultValue:null,description:"",name:"spaceU",required:!1,type:{name:"enum",value:[{value:'"em"'},{value:'"px"'},{value:'"ex"'},{value:'"ch"'},{value:'"rem"'},{value:'"vw"'},{value:'"vh"'},{value:'"vmin"'},{value:'"vmax"'},{value:'"vb"'},{value:'"vi"'},{value:'"svw"'},{value:'"svh"'},{value:'"lvw"'},{value:'"lvh"'},{value:'"dvw"'},{value:'"dvh"'}]}},subColumn:{defaultValue:null,description:"",name:"subColumn",required:!1,type:{name:"enum",value:[{value:'"first"'},{value:'"last"'}]}},mainMinWidthPer:{defaultValue:null,description:"",name:"mainMinWidthPer",required:!1,type:{name:"number"}},subMaxWidthV:{defaultValue:null,description:"",name:"subMaxWidthV",required:!1,type:{name:"number"}},subMaxWidthU:{defaultValue:null,description:"",name:"subMaxWidthU",required:!1,type:{name:"enum",value:[{value:'"em"'},{value:'"px"'},{value:'"ex"'},{value:'"ch"'},{value:'"rem"'},{value:'"vw"'},{value:'"vh"'},{value:'"vmin"'},{value:'"vmax"'},{value:'"vb"'},{value:'"vi"'},{value:'"svw"'},{value:'"svh"'},{value:'"lvw"'},{value:'"lvh"'},{value:'"dvw"'},{value:'"dvh"'}]}},noStretchHeight:{defaultValue:null,description:"",name:"noStretchHeight",required:!1,type:{name:"boolean"}},theme:{defaultValue:null,description:"",name:"theme",required:!1,type:{name:"any"}},as:{defaultValue:null,description:"",name:"as",required:!1,type:{name:"undefined"}},forwardedAs:{defaultValue:null,description:"",name:"forwardedAs",required:!1,type:{name:"undefined"}}}},typeof STORYBOOK_REACT_CLASSES<"u"&&(STORYBOOK_REACT_CLASSES["src/components/TwoColumn/LayoutContainer.tsx#LayoutContainer"]={docgenInfo:s.__docgenInfo,name:"LayoutContainer",path:"src/components/TwoColumn/LayoutContainer.tsx#LayoutContainer"})}catch{}const _=["first","last"],V={subColumn:l("first"),mainMinWidthPer:l(60),spaceV:l(1.7),spaceU:l("rem"),subMaxWidthV:l(15),subMaxWidthU:l("rem"),noStretchHeight:l(!1)},a=y(V),T=({ref:e,as:r,children:h,...f}={...a})=>t(s,{...f,ref:e,as:r||"div",children:h}),u=m.exports.forwardRef(T);try{u.displayName="TwoColumn",u.__docgenInfo={description:"",displayName:"TwoColumn",props:{spaceV:{defaultValue:null,description:"",name:"spaceV",required:!1,type:{name:"number"}},spaceU:{defaultValue:null,description:"",name:"spaceU",required:!1,type:{name:"enum",value:[{value:'"em"'},{value:'"px"'},{value:'"ex"'},{value:'"ch"'},{value:'"rem"'},{value:'"vw"'},{value:'"vh"'},{value:'"vmin"'},{value:'"vmax"'},{value:'"vb"'},{value:'"vi"'},{value:'"svw"'},{value:'"svh"'},{value:'"lvw"'},{value:'"lvh"'},{value:'"dvw"'},{value:'"dvh"'}]}},subColumn:{defaultValue:null,description:"",name:"subColumn",required:!1,type:{name:"enum",value:[{value:'"first"'},{value:'"last"'}]}},mainMinWidthPer:{defaultValue:null,description:"",name:"mainMinWidthPer",required:!1,type:{name:"number"}},subMaxWidthV:{defaultValue:null,description:"",name:"subMaxWidthV",required:!1,type:{name:"number"}},subMaxWidthU:{defaultValue:null,description:"",name:"subMaxWidthU",required:!1,type:{name:"enum",value:[{value:'"em"'},{value:'"px"'},{value:'"ex"'},{value:'"ch"'},{value:'"rem"'},{value:'"vw"'},{value:'"vh"'},{value:'"vmin"'},{value:'"vmax"'},{value:'"vb"'},{value:'"vi"'},{value:'"svw"'},{value:'"svh"'},{value:'"lvw"'},{value:'"lvh"'},{value:'"dvw"'},{value:'"dvh"'}]}},noStretchHeight:{defaultValue:null,description:"",name:"noStretchHeight",required:!1,type:{name:"boolean"}},as:{defaultValue:null,description:"",name:"as",required:!1,type:{name:"ElementType<any>"}},ref:{defaultValue:null,description:"",name:"ref",required:!1,type:{name:"any"}}}},typeof STORYBOOK_REACT_CLASSES<"u"&&(STORYBOOK_REACT_CLASSES["src/components/TwoColumn/index.tsx#TwoColumn"]={docgenInfo:u.__docgenInfo,name:"TwoColumn",path:"src/components/TwoColumn/index.tsx#TwoColumn"})}catch{}const S={subColumn:{control:{type:"radio"},options:_,description:"A string indicating which of the two child elements should be considered a sidebar",table:{type:{summary:null},category:"style",defaultValue:{summary:a.subColumn,details:null}}},mainMinWidthPer:{...n.minWidth,control:{type:"number"},description:"Minimum width of main content in horizontal arrangement (%)",table:{...n.minWidth.table,defaultValue:{summary:a.mainMinWidthPer,details:null}}},noStretchHeight:{control:{type:"boolean"},description:"Whether the original height of the element should be maintained in vertical placement",table:{type:{summary:null},category:"style",defaultValue:{summary:a.noStretchHeight,details:null}}},subMaxWidthV:{...n.maxWidth,control:{type:"number"},description:"Width of sidebar when side-by-side",table:{...n.maxWidth.table,defaultValue:{summary:a.subMaxWidthV,details:null}}},subMaxWidthU:{...n.maxWidth,control:{type:"select"},options:d.Unit.length,description:"Unit of subMaxWidthV",table:{...n.maxWidth.table,defaultValue:{summary:a.subMaxWidthU,details:null}}},spaceV:{...n.gap,control:{type:"number"},description:"Gap between sidebar and main content",table:{...n.gap.table,defaultValue:{summary:a.spaceV,details:null}}},spaceU:{...n.gap,control:{type:"select"},options:d.Unit.length,description:"Unit of gapV",table:{...n.gap.table,defaultValue:{summary:a.spaceU,details:null}}}},$={title:"layout/TwoColumn",component:u,parameters:{storySource:{source:`import { ComponentStory } from '@storybook/react'
+import { TwoColumn } from '../components/TwoColumn'
+import { defaultProps } from '../components/TwoColumn/model'
+import { desc_TwoColumnProps } from './args/TwoColumn'
+import { desc_as } from './args/common/as'
+import { desc_children } from './args/common/children'
+import { desc_ref } from './args/common/ref'
+import styled from 'styled-components'
+import React, { memo } from 'react'
+import { LoremIpsum } from 'react-lorem-ipsum'
+
+export default {
+  title: 'layout/TwoColumn',
+  component: TwoColumn,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Suitable solution to achieve a responsive layout with a sidebar on one side'
+      }
+    }
+  },
+  argTypes: {
+    ...desc_as,
+    ...desc_ref,
+    ...desc_children,
+    ...desc_TwoColumnProps
+  }
+}
+
+const NeedScrollContainer = styled.div\`
+  height: 200vh;
+  color: #4d608b;
+\`
+
+const Column1 = styled.div\`
+  box-shadow: rgba(0, 0, 0, 0.04) 0px 3px 5px;
+  background-color: #a2a7fe66;
+\`
+
+const Column2 = styled.div\`
+  box-shadow: rgba(0, 0, 0, 0.04) 0px 3px 5px;
+  background-color: #ffa6a666;
+\`
+
+const Column1Content = memo(() => <LoremIpsum />)
+const Column2Content = memo(() => <LoremIpsum p={3} />)
+
+const Template: ComponentStory<typeof TwoColumn> = ({ children, ...args }) => (
+  <TwoColumn {...args} as={NeedScrollContainer}>
+    <Column1>
+      <Column1Content />
+    </Column1>
+    <Column2>
+      <Column2Content />
+    </Column2>
+  </TwoColumn>
+)
+
+export const playground = Template.bind({})
+playground.args = {
+  ...defaultProps
+}
+`,locationsMap:{playground:{startLoc:{col:51,line:49},endLoc:{col:1,line:58},startBody:{col:51,line:49},endBody:{col:1,line:58}}}},docs:{description:{component:"Suitable solution to achieve a responsive layout with a sidebar on one side"}}},argTypes:{...C,...x,...w,...S}},W=i.div`
+  height: 200vh;
+  color: #4d608b;
+`,M=i.div`
+  box-shadow: rgba(0, 0, 0, 0.04) 0px 3px 5px;
+  background-color: #a2a7fe66;
+`,L=i.div`
+  box-shadow: rgba(0, 0, 0, 0.04) 0px 3px 5px;
+  background-color: #ffa6a666;
+`,q=m.exports.memo(()=>t(v.LoremIpsum,{})),U=m.exports.memo(()=>t(v.LoremIpsum,{p:3})),O=({children:e,...r})=>b(u,{...r,as:W,children:[t(M,{children:t(q,{})}),t(L,{children:t(U,{})})]}),P=O.bind({});P.args={...a};const N=["playground"];export{N as __namedExportsOrder,$ as default,P as playground};
+//# sourceMappingURL=TwoColumn.stories.58ecf3a6.js.map
