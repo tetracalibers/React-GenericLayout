02decf9f6f1a5569398b432c5f4be668886b44ad
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-23 21:23:40 +0900

    github-pagesデプロイツール削除

--- storybook-static/assets/es.map.constructor.ba9d875b.js
+++
@@ -1,2 +0,0 @@
-import{n as o,o as t}from"./iframe.6deac106.js";var r=o,e=t;r("Map",function(n){return function(){return n(this,arguments.length?arguments[0]:void 0)}},e);
-//# sourceMappingURL=es.map.constructor.ba9d875b.js.map
